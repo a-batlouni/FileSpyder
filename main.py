2926c4dc90cf4a512a2f4ae9dcf0743a226e53c2
a-batlouni <69777867+a-batlouni@users.noreply.github.com>
2020-09-22 00:56:40 -0400

    Created a class Created init and crawl function

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import os
+import pathlib
 
 
-def dirDict(path):
-    emptyList=[]
-    # Returns contents of current working dir
-    dirList = os.listdir(path)
-    # Checks to see if each content of working dir is a dir
-    for contents in dirList:
-        emptyList.append(os.path.isdir(contents))
-    # Combines lists to create dictionary with file name as key and isDir as value
-    dirDict=dict(zip(dirList,emptyList))
-    return(dirDict)
+class Spyder:
+    def __init__(self, path):
+        self.path=path
+        self.dirDict=None
 
+    def makeDict(self):
+        emptyList=[]
+        # Returns contents of current working dir
+        dirList = os.listdir(self.path)
+        # Checks to see if each content of working dir is a dir
+        for contents in dirList:
+            emptyList.append(os.path.isdir(contents))
+        # Combines lists to create dictionary with file name as key and isDir as value
+        self.dirDict=dict(zip(dirList,emptyList))
+        print(self.dirDict)
+        return(self.dirDict)
+    
+    def crawl(self):
+        for key, value in self.dirDict.items():
+            if value is True:
+                print("This is a dir")
+            else:
+                print('This is a file')
+        
+x = Spyder(".")
 
-print(dirDict("."))
-
+x.makeDict()
+x.crawl()
 
 
